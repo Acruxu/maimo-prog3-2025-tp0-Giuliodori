6dc593ffe30b7b6e95f72820e1b395a3f29ce0e2
Juan Cruz <giuliodori.juancruz03@gmail.com>
2025-04-10 19:02:01 -0300

    cambios en el css y el recipe.js

--- assets/js/recipe.js
+++ assets/js/recipe.js
@@ -1,26 +1,33 @@
 const main = async () => {
-    const searchParams = new URLSearchParams(window.location.search);
-    console.log(searchParams.get('id')); // price_descending
-    const id = searchParams.get('id');
+  const searchParams = new URLSearchParams(window.location.search);
+  console.log(searchParams.get("id"));
+  const id = searchParams.get("id");
 
-    const $content = document.querySelector('.single_content');
-    const response = await fetch(`https://dummyjson.com/recipes/${id}`)
-    const responseData = await response.json();
+  const $content = document.querySelector(".single_content");
+  const response = await fetch(`https://dummyjson.com/recipes/${id}`);
+  const responseData = await response.json();
 
-    console.log(responseData);
+  console.log(responseData);
 
-    $content.innerHTML = 
-    `
+  $content.innerHTML = `
         <h1>
             ${responseData.name}
         </h1>
         <img src="${responseData.image}" alt="${responseData.name}"/>
-        <ul>
-        ${responseData.ingredients.map(ingredient => `<li>${ingredient}</li>`)
-            .join("")}
+            <h3>Ingredients</h3>
+        <ul class="ingredients">
+        ${responseData.ingredients
+          .map((ingredient) => `<li>${ingredient}</li>`)
+          .join("")}
         </ul>
-    `
-}
-
+        <h2>Instructions</h2>
+        <ul class="instrucciones">
+        ${responseData.instructions
+          .map((instruction) => `<li>${instruction}</li>`)
+          .join("")
+        }
+        </ul>
+    `;
+};
 
 main();
